7df177ea411754a9035699488c362d70966e92e9
AlanLyu1992 <lyu.y@wustl.edu>
2017-06-15 12:22:15 -0500

    spring-mvc-demo

---
+++ src/com/alanlyu/service/demo/GenericWelcomeService.java
@@ -0,0 +1,7 @@
+package com.alanlyu.service.demo;
+
+import java.util.List;
+
+public interface GenericWelcomeService {
+	public List<String> getWelcomeMessage(String name);
+}
---
+++ src/com/alanlyu/springdemo/controller/WelcomeController.java
@@ -0,0 +1,35 @@
+package com.alanlyu.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.alanlyu.service.demo.GenericWelcomeService;
+import com.alanlyu.service.demo.WelcomeService;
+
+@Controller
+public class WelcomeController {
+	
+	@Autowired
+	private GenericWelcomeService welcomeService;
+	
+	
+	
+	@RequestMapping("/")
+	//http://localhost:8080/spring-mvc-demo-1/
+	public String doWelcome(Model model){
+		
+		//1. Retrieving the processed data
+//		WelcomeService welcomeService = new WelcomeService();
+		List<String> welcomeMessage = welcomeService.getWelcomeMessage("Alan");
+		
+		//2. Add data to the model
+		model.addAttribute("myWelcomeMessage",welcomeMessage);
+		
+		//3. return logical view name
+		return "welcomeNew";
+	}
+}
